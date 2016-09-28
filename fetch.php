c8872ccd3c66468ceeee4b9639f57bfc8ec2fb61
Alexander Tallqvsit <alexander_tallqvist@hotmail.com>
2016-09-28 16:44:57 +0300

    Added dynamic league outputs

--- fetch.php
+++ fetch.php
@@ -3,9 +3,12 @@
 $sport = $_POST['sport'];
 $date  = $_POST['date'];
 $output = "";
+$leaguesStart = "<select><option value='leagues'>Choose a league</option>";
+$leaguesArray = array();
 $results = 0;
 
-$jsondata = file_get_contents("http://alexandertallqvist.net/sportdata.json");
+//$jsondata = file_get_contents("http://alexandertallqvist.net/sportdata.json");
+$jsondata = file_get_contents("sportdata.json");
 $array = json_decode($jsondata, true);
 
 foreach ($array as $index => $item) {
@@ -14,6 +17,8 @@ foreach ($array as $index => $item) {
       foreach($item as $key => $value) {
         $output.= $key . ": " . $value . "<br>";
       }
+      $leaguesString = "<option value='" . $item['league'] . "'>" .  $item['league'] . "</option>";
+      if(!in_array($leagueString, $leaguesArray)){array_push($leaguesArray, $leaguesString);};
       $results ++;
       $output.= "<br><br>";
     }
@@ -22,16 +27,23 @@ foreach ($array as $index => $item) {
       foreach($item as $key => $value) {
         $output.= $key . ": " . $value . "<br>";
       }
+      $leaguesString = "<option value='" . $item['league'] . "'>" .  $item['league'] . "</option>";
+      if(!in_array($leaguesString, $leaguesArray)){array_push($leaguesArray, $leaguesString);};
       $results ++;
       $output.= "<br><br>";
     }
   }
 }
 
+$leaguesMiddle = join("", $leaguesArray);
+$leaguesEnd = "</select>";
+$leaguesOutput = $leaguesStart . $leaguesMiddle . $leaguesEnd;
+
 if($results == 0){
   echo "<h3>"  . $results . " results found" . "</h3>";
   echo "No data found with the selected values. Try changing some of the values and try again.";
 }else{
+  echo $leaguesOutput;
   echo "<h3>"  . $results . " results found" . "</h3>";
   echo $output;
 }
--- teams.php
+++ teams.php
@@ -4,7 +4,7 @@ $jsondata = file_get_contents("teams.json");
 $array = json_decode($jsondata, true);
 $output = "";
 
-$output = "<select><option value='team'>Välj ett team</option>";
+$output = "<select><option value='team'>Choose a team</option>";
 
 foreach ($array as $index => $item) {
   $output .= "<option value='" . $item['sport'] . "'>" .  $item['team'] . "</option>";
