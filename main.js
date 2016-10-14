3b95c666166224833a97147a8c6bd30fe9677842
Alexander Tallqvist <alexander.tallqvist@hotmail.com>
2016-10-14 23:05:49 +0300

    Made the tab color changing more efficient

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
 
-  //Asign containers to variables
+  // Asign containers to variables
   var teamContainer     = $('.team-container select');
   var leagueContainer   = $('.league-container select');
   var resultsContainer  = $('.result-container .result');
@@ -9,19 +9,19 @@ $(document).ready(function(){
   var errorContainer    = $('.error-container');
   var loadingContainer  = $('.loading');
 
-  //Emppty all the containers
+  // Emppty all the containers
   scoreContainer.empty();
   errorContainer.empty();
   resultsContainer.empty();
 
-  //Check the default radio button
+  // Check the default radio button
   $("#fotboll").prop("checked", true);
 
-  //Get data when page is loaded
+  // Get data when page is loaded
   getTeamData();
 
-  //Ajax function that gets the sports data
-  function getScoreData(){
+  // Ajax function that gets the sports data
+  function getScoreData(color){
     loadingContainer.show();
 
     var sport  = $('input[name=sport]:checked').val();
@@ -68,7 +68,7 @@ $(document).ready(function(){
           "<div class='error alert alert-info'>No values found. Try resetting your filters." +
           "<br><br><button class='reset btn btn-primary'>Reset Filters</button></div>"
         );}
-
+       changeColor(color);
        loadingContainer.delay(300).fadeOut();
       }
     });
@@ -76,7 +76,7 @@ $(document).ready(function(){
 
 
   //Ajax function that gets the teams and the leagues
-  function getTeamData(){
+  function getTeamData(color){
     var sport  = $('input[name=sport]:checked').val();
 
     $.ajax({
@@ -89,13 +89,13 @@ $(document).ready(function(){
         $('.league-container select option[value!="League"]').remove();
         teamContainer.append(data[0]);
         leagueContainer.append(data[1]);
-        getScoreData();
+        getScoreData(color);
       }
     });
   }
 
 
-  //Initiate the datepicker
+  // Initiate the datepicker
   $(function(){
     $("#datepicker").datepicker({
       minDate: -20,
@@ -105,13 +105,7 @@ $(document).ready(function(){
   });
 
 
-  // Change table header depending on sport
-  $("label[for=fotboll]" ).click(function(){ChangeColor("#81e0a9");});
-  $("label[for=korgboll]").click(function(){ChangeColor("#f0b17a");});
-  $("label[for=ishockey]").click(function(){ChangeColor("#85c1e9");});
-  $("label[for=afotboll]").click(function(){ChangeColor("#f0938a");});
-
-  function ChangeColor(color) {
+  function changeColor(color) {
     setTimeout(function(){
       $('.table-header').css("background", color);
     },300);
@@ -119,45 +113,45 @@ $(document).ready(function(){
 
 
   //Get new data when the sport is changed
+  // Get the paramater for changing the color
   $(document).on('change', 'input[name=sport]', function() {
-    getTeamData();
+    var color = $(this).data('color');
+    getTeamData(color);
   });
 
 
-  //Get new data when the date is changed
+  // Get new data when the date is changed
   $(document).on('change', 'input[name=date]', function() {
     getScoreData();
   });
 
 
-  //Refresh date when the date is removed
+  // Refresh date when the date is removed
   $(document).on('click', '#clear-date', function() {
     $('input[name=date]').val("Date");
     getScoreData();
   });
 
 
-  //Get new data when the team is changed
+  // Get new data when the team is changed
   $(document).on('change', '.team-dropdown', function() {
     getScoreData();
   });
 
 
-  //Get new data when the league is changed
+  // Get new data when the league is changed
   $(document).on('change', '.league-dropdown', function() {
     getScoreData();
   });
 
 
-  //Function for resetting the filters
+  // Function for resetting the filters
   $(document).on('click', '.reset', function() {
     $("#fotboll").prop("checked", true);
     $('input[name=date]').val('Date');
     $('.league-dropdown').val('League');
     $('.team-dropdown').val('Team');
-
-    getScoreData();
-    ChangeColor("#81e0a9");
+    getScoreData("#81e0a9");
   });
 
 
